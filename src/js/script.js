14c921d8004e85aa7eed6d5604fb5672ab7aa03c
Miguel <miguelbrandao781@gmail.com>
2026-09-09 12:03:21 -0300

    feat: Funcionalidade do slideshow

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,34 @@
+// ARRAY DE IMAGENS
+let imagens=[
+    "src/assets/carro1.jpeg",
+    "src/assets/carro2.jpeg",
+    "src/assets/carro3.jpeg"
+]
+
+//POSIÇÃO QUE VAI INICIAR AS IMAGENS
+let index=0;
+//TEMPO PARA MUDAR IMAGEM
+let tempo=3000; //3 segundos
+
+//FUNÇÃO DO SLIDESHOW
+function SlideShow() {
+    // DOM - PEGA O ID E PASSA O CAMINHO DAS IMAGENS
+    document.getElementById("imgBanner").src=imagens[index];
+    // INCREMENTO DAS IMAGENS
+    index++;
+    // ESTRUTURA CONDICIONAL IF(SE A POSIÇÃO FOR IGUAL A IMAGEM)
+    if(index == imagens.length){
+        // INICIA NA POSIÇÃO 0( OU SEJA, SEMPRE VOLTA O 0)
+        index = 0;
+    }
+
+    // MÉTODO SETIMEOUT PARA EXECUTAR A FUNÇÃO (CALLBACK) E
+    // CHAMAR O TEMPO
+    setTimeout("SlideShow()", tempo);
+}
+//EXECUTAR A FUNÇÃO
+SlideShow()
+
 const menuIcone = document.getElementById("menu-icone");
 const navMenu = document.getElementById("nav-menu");
 
